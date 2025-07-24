cb0f20208b7b3b1bdd29de4f5496928865d87000
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-07-24 08:18:09 +0800

    :green_heart: Use os.getenv()

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,17 @@ includes("packages/c/cppinsights")
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
-add_requires("cppinsights", "llvm", "clang")
+add_requires("cppinsights", "llvm")
 
 target("cppinsights")
 do
     add_rules("lua.module")
     add_files("*.cpp", "*.c")
-    add_packages("cppinsights", "llvm", "clang")
+    add_packages("cppinsights", "llvm")
+    if os.getenv "CFLAGS" then
+        add_cflags(os.getenv "CFLAGS")
+    end
+    if os.getenv "LDFLAGS" then
+        add_ldflags(os.getenv "LDFLAGS")
+    end
 end
